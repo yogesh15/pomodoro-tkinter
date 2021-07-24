7fb709ec1245efbd52df322013e3a50543801a41
Yogesh Panjwani <panjwaniyogesh95@gmail.com>
2021-07-24 10:23:23 +0530

    timer mechanism basic is added and how to change canvas element is shown

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    #To change the canvas element, we do it as it is done below
+    canvas.itemconfig(timer_text, text=count)
+    if count > 0:
+        window.after(1000, count_down, count-1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -21,11 +26,27 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+title_label.grid(column=1, row=0)
+title_label.config(padx=20, pady=20)
+
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 100, image=tomato_img)
-canvas.create_text(100, 120, text="00:00", fill="white", font=(FONT_NAME, 30, "bold"))
-canvas.pack()
+timer_text = canvas.create_text(100, 120, text="00:00", fill="white", font=(FONT_NAME, 30, "bold"))
+canvas.grid(column=1, row=1)
+
+count_down(5)
+
+button = Button(text="Start", bg=YELLOW, highlightthickness=0)
+button.grid(column=0, row=2)
+
+button = Button(text="Reset", bg=YELLOW, highlightthickness=0)
+button.grid(column=2, row=2)
+
+check_marks = Label(text="✔", fg=GREEN, bg=YELLOW)
+check_marks.grid(column=1, row=3)
 
 window.mainloop()
 
